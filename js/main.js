fa31373c8290c6192be1bcad9a2407d55a5c20bd
R1d22 <rydsic@gmail.com>
2021-04-28 19:11:45 +0300

    gotlvl3hardly

--- js/main.js
+++ js/main.js
@@ -1,149 +1,157 @@
-/*const getValue = () => {
-    const formEl = document.forms.roof;
-
-    const data = new FormData(formEl);
-
-    const one = Number(data.get('width').trim());
-    const two = Number(data.get('height').trim());
-    return [one,two];
-};
-
-const setResult = (val) => {
-    const container = document.querySelector('.container')
-    container.innerHTML = val;
-};
-const multiplyBtn = document.querySelector('.multiply');
-const block = document.querySelector('.block');
-const table = document.querySelector('.no-display');
-
-//const tbody = document.querySelector('tbody');
+const mainModel = [];
+const pos = {x: null, y: null};
 
 
-multiplyBtn.addEventListener('click', (ev) => {
-    ev.preventDefault();
-
-    
-   // block.classList.add("none")
-    //table.classList.remove("no-display");
-
-    const createField = (one,two) => {
-        let html = '';
-        
-
-        for(let i = 0; i < one; i++) {
-            html += '<div class="block">';
-            for(let j = 0; j < two; j++) {
-                html = '</div>'
-            }
-        }
-    }
-    createField(one,two);
-
-    setResult.innerHTML = html;
-
-
-    /*const vals = getValue();
-    const result = vals[0]// * vals[1] ;
-    //const block = document.createElement('div');
-
-    //table.classList.add("block");
-    /*for (let i=a; i<vals[0]; i++) {
-        result = result + i;
-    }
-
-    /*if(vals[0] > block) {
-        div.clone(block);
-        block.classList.remove("none");
-        block.classList.add("block");
-       
-    }  else {return}*/
-
-    /*for(let i = result; result < vals[0]; i++) {
-        result = result.clone(block) + i;
-    }*/
-
-   
-
- /*   setResult(result);
-});
- 
-
-
-
-
-
-
-
-
-
-
-
-
-//const block = document.querySelector('.block');
-/*const secondContainer = document.querySelector('.second__container')
-function a(numberSelected) {
-    let numberOne = 0;
-    for (let i = 0; i < one; i++){
-        if (numberSelected.one[i].selected) {
-            numberOne
-        }
-    }
-    return numberOne
-}/*
-
-
-multiplyBtn.addEventListener('click', function(){
-    alert
-})
-/*multiplyBtn.addEventListener('click', (ev) => {
-    ev.preventDefault();
-
-    const vals = getValue();
-    const result = vals[0] * vals[1];
-    /*draw = function() {
-        typed = $('#width').val()
-    
-    }*/
-    //const divBlock = document.createElement('div');
-    //divBlock.classList.add('block')
-
-   //setResult(result);
-    /*const html = `
-        <div>
-             ${divBlock}
-        </div>
-    `
-    secondContainer.insertAdjacentHTML('afterbegin', html);*/
-//});
-
-
-//const submitButton = document.querySelector('.submit');
 const container = document.querySelector('.container');
 const submitButton = document.querySelector('.submit-button');
 const block = document.querySelector('.no-display');
+const form = document.querySelector('.form');
+const joystickFrom = document.querySelector('.joystick');
+const upJoystickBtn = document.querySelector('.up');
+const rightJoystickBtn = document.querySelector('.right');
+const downJoystickBtn = document.querySelector('.down');
+const leftJoystickBtn = document.querySelector('.left');
 
-submitButton.addEventListener('click', (ev) => {
-    
-    const formData = new FormData();
-    const column = Number(formData.get('.one'));
-    const row = Number(formData.get('.two'));
-    
-    
 
-    createField(column, row);
-});
+
 
 
 const createField = (column, row) => {
     let html = '';
 
     for(let i = 0; i < row; i++){
-        html += `<div class="block">`;
+        html += `<div class="row">`;
+        mainModel[i] = []
         for(let j = 0; j < column; j++){
             html += `<div class="block"></div>`;
+            mainModel[i][j] = null;
         }
         html += `</div>`;
     };
-    console.log('html:' +html)
     container.innerHTML = html;
+    
 };
+
+const getPos = () => {
+    return {x: pos.x, y: pos.y};
+};
+
+const setPos = (x, y) => {
+    pos.x = x;
+    pos.y = y;
+    render()
+};
+const up = () => {
+    const curPos = getPos();
+    const newY = curPos.y - 1;
+
+    if(mainModel[newY] === undefined || mainModel[newY][curPos.x] === undefined) {
+        return false;
+    }
+
+    setPos(curPos.x, newY);
+    render()
+};
+
+const right = () => {
+    const curPos = getPos();
+    const newX = curPos.x + 1;
+
+    if(mainModel[newX] === undefined || mainModel[curPos.y][newX] === undefined) {
+        return false
+    };
+
+    setPos(newX, curPos.y);
+    render()
+};
+
+const down = () => {
+    const curPos = getPos();
+    const newY = curPos.y + 1;
+
+    if(mainModel[newY] === undefined || mainModel[newY][curPos.x] === undefined) {
+        return false;
+    };
+
+    setPos(curPos.x, newY);
+    render()
+};
+
+const left = () => {
+    const curPos = getPos();
+    const newX = curPos.x - 1;
+
+    if(mainModel[newX] === undefined || mainModel[newX][curPos.y] === undefined) {
+        return false
+    };
+    
+    setPos(newX, curPos.y);
+    render()
+};
+
+const render = () => {
+    const curPos = getPos();
+    const sel = `.container div:nth-child(${curPos.y+1}) div:nth-child(${curPos.x+1})`;
+
+    const cell = document.querySelector(sel);
+
+    const oldActiveCell = document.querySelector('.active-color');
+    if(oldActiveCell) {
+        oldActiveCell.classList.remove('active-color');
+    };
+    cell.classList.add('active-color');
+};
+
+
+upJoystickBtn.addEventListener('mousedown', (ev) => {
+    up()
+});
+document.addEventListener('keydown', (ev) => {
+    if(ev.code !== 'ArrowUp') {
+        return 
+    }
+   
+    up()
+});
+rightJoystickBtn.addEventListener('mousedown', (ev) => {
+    right()
+});
+document.addEventListener('keydown', (ev) => {
+    if(ev.code !== 'ArrowRight') {
+        return
+    }
+    right()
+});
+
+downJoystickBtn.addEventListener('mousedown', (ev) => {
+    down()
+});
+document.addEventListener('keydown', (ev) => {
+    if(ev.code !== 'ArrowDown') {
+        return
+    }
+    down()
+});
+leftJoystickBtn.addEventListener('mousedown', (ev) => {
+    left()
+});
+document.addEventListener('keydown', (ev) => {
+    if(ev.code !== 'ArrowLeft') {
+        return
+    }
+    left()
+});
+
+
+submitButton.addEventListener('click', () => {
+    
+    const formData = new FormData(form);
+    const column = Number(formData.get('width'));
+    const row = Number(formData.get('height'));
+    
+    
+
+    createField(column, row);
+    setPos(2,2);
+    console.log(mainModel)
+});
